f2008e422d260785ff039a0a4b3a4206214e417d
Naxtra <pietervandeschoot@live.nl>
2015-09-23 21:30:26 +0200

    opdracht 1 van week 2

--- Prog5-master/Prog5_Opdrachten_Week2/CsharpCity/Thief.cs
+++ Prog5-master/Prog5_Opdrachten_Week2/CsharpCity/Thief.cs
@@ -13,7 +13,8 @@ namespace CsharpCity.Models
         //1. We moeten door de poort met tralies
         public void OpenGate()
         {
-            
+            Key key = new Key("key");
+            bank.Gate.Sleutelgat = key;
             //bank.Guard;
             //bank.Gate.Sleutelgat
         }
@@ -21,18 +22,22 @@ namespace CsharpCity.Models
         //2. Verzin een manier om de Kluis open te maken
         public void OpenVault()
         {
+            bank.Vault.ThisPropertyDoesNothing = "ReallyNothing";
             //bank.Vault
         }
 
         //3. Verzin een manier om de lazors uit te schakelen
         public void DodgeLazors()
         {
+            bank.Lazors.ForEach(lazer => lazer.Active = false);
             //bank.Lazors
         }
 
         //4. Er is 1 kluisje met daarin juwel die ultra veel waard zijn. Hoe kom je er achter welke?
         public void FindAndOpenSafe()
         {
+            //van uitwerking
+            bank.SafeList.OrderBy(money => money.Money).First().OpenSafe();
             //bank.SafeList
         }
 
