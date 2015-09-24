97ab6172d2ff6014af351b5d876ab11e81ad1e54
Naxtra <pietervandeschoot@live.nl>
2015-09-24 13:57:22 +0200

    Begin van week 2 opdr 3

--- Prog5-master/Prog5_Opdrachten_Week2/TheRepository.Test/MyFirstTestClass.cs
+++ Prog5-master/Prog5_Opdrachten_Week2/TheRepository.Test/MyFirstTestClass.cs
@@ -11,12 +11,17 @@ namespace TheRepository.Test
         public void MyFirstTestMethode()
         {
             //1. Arrange
-
+            var car = new CarVM(new Car { Prijs = 134, Bijtelling = 14 });
+            var expected = 134 / 14 * 2;
+            
             //2. Act
+            var result = car.LeaseKostenPerMaand;
 
             //3. Assert
-
+            Assert.AreEqual(expected, result);
         }
+
+
     }
 
 }
